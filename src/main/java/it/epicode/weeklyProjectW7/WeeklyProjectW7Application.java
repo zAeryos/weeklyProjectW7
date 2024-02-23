29624849848a70b1a8de6f30e2c99ec5d6133cdc
zAeryos <dave_13_08@hotmail.it>
2024-02-23 13:31:29 +0100

    First commit

---
+++ src/main/java/it/epicode/weeklyProjectW7/WeeklyProjectW7Application.java
@@ -0,0 +1,13 @@
+package it.epicode.weeklyProjectW7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyProjectW7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyProjectW7Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/weeklyProjectW7/WeeklyProjectW7ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.weeklyProjectW7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyProjectW7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
